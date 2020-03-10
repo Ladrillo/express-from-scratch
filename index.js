d30b4fca8e4082e8dabf6c2d1e89d138df8160a6
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-10 11:41:07 +0100

    wip

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/friends', (req, res) => {
 
 app.post('/api/friends', (req, res) => {
   const { friend } = req.body
-  // THE DIFFERENT WAYS CLIENT CAN SEND INFO TO SERVER !!!
+  // THE DIFFERENT WAYS CLIENT CAN SEND INFO TO SERVER
   // const { body } = req // REMEMBER TO `app.use(express.json())` !!!
   // const { params } = req
   // const { headers } = req

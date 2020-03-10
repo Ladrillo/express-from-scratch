3a8f87472ec991ec347397cd0b98e94a43fb7bb1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-10 11:57:44 +0100

    wip

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
-// import express app and cors
-const express = require('express')
-const cors = require('cors')
-
-// instantiate the app
-const app = express()
-
-// configure app so responses
-// carry special headers to enable CORS
-app.use(cors())
-// by default express cannot parse bodies of requests
-// we need to opt-in to body-parsing
-// and this comes with express
-app.use(express.json())
-
-const friends = ['Alison', 'Zak', 'Rufai']
-
-app.get('/api/friends', (req, res) => {
-  // THE DIFFERENT WAYS SERVER CAN RESPOND TO CLIENT
-  // res.json()
-  // res.send()
-  // res.end() // no body in the response
-  res.status(200).json(friends)
-})
-
-app.get('/hello', (req, res) => {
-  const { pal } = req.query
-  res.send(`<h1>Hello, ${pal + pal}</h1>`)
-})
-
-app.post('/api/friends', (req, res) => {
-  const { friend } = req.body
-  // THE DIFFERENT WAYS CLIENT CAN SEND INFO TO SERVER
-  // const { body } = req // REMEMBER TO `app.use(express.json())` !!!
-  // const { params } = req
-  // const { headers } = req
-  // const { query } = req
-  friends.push(friend)
-  res.status(201).json(friend)
-})
+const app = require('./apis/server.js')
 
 const PORT = 3000
 
